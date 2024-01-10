2311ca533804a0369bc9705fbbe2c029e3112525
Joachim Noreiko <joachim@107701.no-reply.drupal.org>
2024-01-10 17:30:15 +0000

    docs.

--- src/Entity/TomeFormInterface.php
+++ src/Entity/TomeFormInterface.php
@@ -10,12 +10,37 @@ use Drupal\Core\Entity\EntityWithPluginCollectionInterface;
  */
 interface TomeFormInterface extends ConfigEntityInterface, EntityWithPluginCollectionInterface {
 
+  /**
+   * Gets the ID of the form this entity handles.
+   *
+   * @return string
+   *   The form ID.
+   */
   public function getFormId(): string;
 
+  /**
+   * Gets the relative path for the static export form to submit to.
+   *
+   * @return string
+   *   The path, relative to the domain of the static export. Note that Tome
+   *   static exports do not in general work in a subfolder.
+   */
   public function getFormHandlerExportedScriptPath(): string;
 
+  /**
+   * Gets any additional paths to export with Tome.
+   *
+   * @return array
+   *   An array of paths.
+   */
   public function getExportPaths(): array;
 
+  /**
+   * Gets the PHP code for the form handler script.
+   *
+   * @return string
+   *   The PHP code.
+   */
   public function getFormHandlerScriptPhp(): string;
 
 }
