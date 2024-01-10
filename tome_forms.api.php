695e542b1026688e5b88141f7fd9ed687c774b1b
Joachim Noreiko <joachim@107701.no-reply.drupal.org>
2024-01-10 14:11:39 +0000

    more rename

--- tome_forms.api.php
+++ tome_forms.api.php
@@ -22,16 +22,5 @@ function hook_tome_form_handler_info_alter(array &$info) {
 }
 
 /**
- * Perform alterations on Static Form Handler definitions.
- *
- * @param array &$info
- *   Array of information on Static Form Handler plugins.
- */
-function hook_static_form_handler_info_alter(array &$info) {
-  // Change the class of the 'foo' plugin.
-  $info['foo']['class'] = SomeOtherClass::class;
-}
-
-/**
  * @} End of "addtogroup hooks".
  */
