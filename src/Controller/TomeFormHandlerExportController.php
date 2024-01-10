cb8f0e41c95ba453be01ac2764a82cb2e486f1b6
Joachim Noreiko <joachim@107701.no-reply.drupal.org>
2024-01-10 15:33:01 +0000

    simplify controllers

---
+++ src/Controller/TomeFormHandlerExportController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Drupal\tome_forms\Controller;
+
+use Drupal\tome_forms\Entity\TomeFormInterface;
+use Symfony\Component\HttpFoundation\Response;
+
+/**
+ * TODO: class docs.
+ */
+class TomeFormHandlerExportController {
+
+  /**
+   * Callback for the tome_forms.tome_form_handler_export route.
+   */
+  public function content(TomeFormInterface $tome_form) {
+    $php = 'Hello!';
+    $response = new Response($php);
+
+    return $response;
+  }
+
+}
