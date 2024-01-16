e41cf8fe6a72af170bf65be943749bb16a67c169
Joachim Noreiko <joachim@107701.no-reply.drupal.org>
2024-01-16 16:39:07 +0000

    NOO

--- src/Plugin/TomeFormSecurityHandler/FormToken.php
+++ src/Plugin/TomeFormSecurityHandler/FormToken.php
@@ -3,9 +3,12 @@
 namespace Drupal\tome_forms\Plugin\TomeFormSecurityHandler;
 
 use Drupal\Component\Utility\Crypt;
+use Drupal\Component\Datetime\TimeInterface;
 use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
 use Drupal\tome_forms\Entity\TomeFormInterface;
 use Drupal\tome_forms\Entity\TomeFormSecurityInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * TODO: class docs.
@@ -15,17 +18,62 @@ use Drupal\tome_forms\Entity\TomeFormSecurityInterface;
  *   label = @Translation("Form token"),
  * )
  */
-class FormToken extends TomeFormSecurityHandlerBase {
+class FormToken extends TomeFormSecurityHandlerBase implements ContainerFactoryPluginInterface {
+
+  /**
+   * The time service.
+   *
+   * @var \Drupal\Component\Datetime\TimeInterface
+   */
+  protected $time;
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    return new static(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      $container->get('datetime.time'),
+    );
+  }
+
+  /**
+   * Creates a FormToken instance.
+   *
+   * @param array $configuration
+   *   A configuration array containing information about the plugin instance.
+   * @param string $plugin_id
+   *   The plugin_id for the plugin instance.
+   * @param mixed $plugin_definition
+   *   The plugin implementation definition.
+   * @param \Drupal\Component\Datetime\TimeInterface $time
+   *   The time service.
+   */
+  public function __construct(
+    array $configuration,
+    $plugin_id,
+    $plugin_definition,
+    TimeInterface $time
+  ) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition);
+    $this->time = $time;
+  }
 
   protected function getFormToken(TomeFormInterface $tome_form): string {
-    // TODO: include timestamp.
-    return 'tome_form_token-' . Crypt::hashBase64($tome_form->id());
+    return 'tome_form_token-' . Crypt::hashBase64($tome_form->id() . $this->time->getRequestTime());
   }
 
   /**
    * {@inheritdoc}
    */
   public function formAlter(&$form, FormStateInterface $form_state, TomeFormInterface $tome_form, TomeFormSecurityInterface $tome_form_security): void {
+    $form['tome_form_timestamp'] = [
+      '#type' => 'token',
+      '#default_value' => $this->time->getRequestTime(), // ARGH won't be the same request time as the PHP script gets!
+    ];
+
     $form['tome_form_token'] = [
       '#type' => 'token',
       // Gets set by the JavaScript.
