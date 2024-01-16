588c6e309d616fef8273de2b3cd5489ade34a0bb
Joachim Noreiko <joachim@107701.no-reply.drupal.org>
2024-01-16 15:00:59 +0000

    form token plugin wip

--- src/Plugin/TomeFormSecurityHandler/FormToken.php
+++ src/Plugin/TomeFormSecurityHandler/FormToken.php
@@ -1,8 +1,11 @@
 <?php
 
-namespace Drupal\tome_forms\Plugin\TomeFormSecurity;
+namespace Drupal\tome_forms\Plugin\TomeFormSecurityHandler;
 
+use Drupal\Component\Utility\Crypt;
 use Drupal\Core\Form\FormStateInterface;
+use Drupal\tome_forms\Entity\TomeFormInterface;
+use Drupal\tome_forms\Entity\TomeFormSecurityInterface;
 
 /**
  * TODO: class docs.
@@ -14,28 +17,29 @@ use Drupal\Core\Form\FormStateInterface;
  */
 class FormToken extends TomeFormSecurityHandlerBase {
 
-  protected function getFormToken(): string {
-    return 'cake';
+  protected function getFormToken(TomeFormInterface $tome_form): string {
+    // TODO: include timestamp.
+    return 'tome_form_token-' . Crypt::hashBase64($tome_form->id());
   }
 
   /**
    * {@inheritdoc}
    */
-  public function formAlter(&$form, FormStateInterface $form_state): void {
+  public function formAlter(&$form, FormStateInterface $form_state, TomeFormInterface $tome_form, TomeFormSecurityInterface $tome_form_security): void {
     // ARH but JS!
     $form['tome_form_token'] = [
       '#type' => 'token',
-      '#default_value' => $this->getFormToken(),
+      '#default_value' => $this->getFormToken($tome_form),
     ];
   }
 
   /**
    * {@inheritdoc}
    */
-  public function getFormHandlerScriptSecurityCheckPhp(): array {
+  public function getFormHandlerScriptSecurityCheckPhp(TomeFormInterface $tome_form, TomeFormSecurityInterface $tome_form_security): array {
     $php_lines = [];
 
-    $form_token = $this->getFormToken();
+    $form_token = $this->getFormToken($tome_form);
 
     $php_lines[] = '// Verify the form token.';
     $php_lines[] = "if (empty(\$_POST['tome_form_token']) || \$_POST['tome_form_token'] != '$form_token' ) { redirect(); }";
