70650a9243ee4b3f590ccd719c986378567b8c02
Joachim Noreiko <joachim@107701.no-reply.drupal.org>
2024-01-17 10:58:59 +0000

    JS uses JSON

--- js/form-token.js
+++ js/form-token.js
@@ -6,10 +6,13 @@
 // Drupal.behaviors is not working for FKW reasons, so ditching it. There's no
 // AJAX anyway!
 (async () => {
+  // Get the form values from the PHP script.
   const response = await fetch(drupalSettings.tome_forms.formTokenPath);
-  const token = await response.text();
-  console.log(token);
+  const json = await response.json();
+  console.log(json);
 
-  document.getElementsByName('tome_form_token')[0].value = token;
+  // Set them on the hidden form elements which were added by hook_form_alter().
+  document.getElementsByName('tome_form_token')[0].value = json['token'];
+  document.getElementsByName('tome_form_timestamp')[0].value = json['timestamp'];
 })()
 
